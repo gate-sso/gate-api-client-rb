1ee07d7b91e6fc7ebeef72fca7c19bb43866de72
Praveen Shukla <praveen.shukla@go-jek.com>
2018-03-11 22:01:52 +0530

    Add specs for configurations

--- spec/gate_api_client/configuration_spec.rb
+++ spec/gate_api_client/configuration_spec.rb
@@ -10,5 +10,16 @@ module GateApiClient
 
       expect(configuration.gate_host).to eq(expected_host)
     end
+
+    describe '.configure' do
+      subject do
+        configuration = GateApiClient.configure do |config|
+          config.gate_host = 'http://localhost:3000'
+        end
+      end
+      it { is_expected.to be_instance_of(described_class) }
+      it { expect(subject.gate_host).to eq('http://localhost:3000') }
+      it { expect(subject.nss_host_path).to eq('/nss/host') }
+    end
   end
 end
