03cd15025f9db13b81c2e01c0eadf33331beeb20
Praveen Shukla <praveen.shukla@go-jek.com>
2018-03-23 10:36:50 +0530

    Restructure the directory

--- lib/gate_api_client.rb
+++ lib/gate_api_client.rb
@@ -2,9 +2,9 @@ require "gate_api_client/version"
 
 require 'ostruct'
 require 'gate_api_client/configuration'
-require 'gate_api_client/nss_host'
-require 'gate_api_client/nss_host_parser'
-require 'gate_api_client/invalid_nss_host'
+require 'gate_api_client/nss_host/nss_host'
+require 'gate_api_client/nss_host/nss_host_parser'
+require 'gate_api_client/nss_host/invalid_nss_host'
 
 module GateApiClient
   CONNECTION_REFUSED  = 'ECONNREFUSED'
